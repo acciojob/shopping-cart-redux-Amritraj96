b4f5f7b54e6cc16ef61913242819bee49a8183ea
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-03-01 20:32:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // Changed from 'react-dom/client'
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import App from './components/App'; // Adjust path based on your folder structure
-import './styles/App.css'; // Make sure this matches your project
+import App from './components/App'; 
+import './styles/App.css'; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 );
